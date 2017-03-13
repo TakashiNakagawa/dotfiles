88cd07c0fa561481c7c5f647a78790ae5efd3456
Takashi Nakagawa <nakagawa@cflat-inc.com>
2017-03-13 21:35:27 +0900

    modify copy.sh

--- copy.sh
+++ copy.sh
@@ -1,6 +1,18 @@
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.gvimrc ~/.gvimrc
-
 mkdir -p ../.vim
-ln -s ~/dotfiles/.vim/neobundle.toml ~/.vim/neobundle.toml
-ln -s ~/dotfiles/.vim/neobundlelazy.toml ~/.vim/neobundlelazy.toml
+
+# We still need this.
+windows() { [[ -n "$WINDIR" ]]; }
+
+
+if windows; then
+  cmd.exe //c "mklink %USERPROFILE%\.vim\neobundle.toml %USERPROFILE%\dotfiles\.vim\neobundle.toml"
+  cmd.exe //c "mklink %USERPROFILE%\.vim\neobundlelazy.toml %USERPROFILE%\dotfiles\.vim\neobundlelazy.toml"
+  cmd.exe //c "mklink %USERPROFILE%\.vimrc %USERPROFILE%\dotfiles\.vimrc"
+  cmd.exe //c "mklink %USERPROFILE%\.gvimrc %USERPROFILE%\dotfiles\.gvimrc"
+else
+  ln -s ~/dotfiles/.vim/neobundle.toml ~/.vim/neobundle.toml
+  ln -s ~/dotfiles/.vim/neobundlelazy.toml ~/.vim/neobundlelazy.toml
+  ln -s ~/dotfiles/.vimrc ~/.vimrc
+  ln -s ~/dotfiles/.gvimrc ~/.gvimrc
+fi
+
