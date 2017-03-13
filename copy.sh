081cbf5757faceaf3ca2b04f2b786e35f0197f08
Takashi Nakagawa <TN@Takashi-no-iMac.local>
2017-03-13 09:19:45 +0900

    add copy script

---
+++ copy.sh
@@ -0,0 +1,6 @@
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.gvimrc ~/.gvimrc
+
+mkdir -p ../.vim
+ln -s ~/dotfiles/.vim/neobundle.toml ~/.vim/neobundle.toml
+ln -s ~/dotfiles/.vim/neobundlelazy.toml ~/.vim/neobundlelazy.toml
